49e8a8cf348b17ce00605a17786a9b4212c2f7df
ZegMaarJoe <willaertjonathan@gmail.com>
2020-03-18 12:34:48 +0100

    test met IsTrue of IsFalse ipv AreEqual

--- TDDCursusLibraryTest/JaarTest.cs
+++ TDDCursusLibraryTest/JaarTest.cs
@@ -9,22 +9,22 @@ namespace TDDCursusLibraryTest
         [TestMethod]
         public void EenJaarDeelbaarDoor400IsEenSchrikkeljaar()
         {
-            Assert.AreEqual(true, new Jaar(2000).IsSchrikkeljaar);
+            Assert.IsTrue(new Jaar(2000).IsSchrikkeljaar);
         }
         [TestMethod]
         public void EenJaarDeelbaarDoor100MaarNietDoor400IsGeenSchrikkeljaar()
         {
-            Assert.AreEqual(false, new Jaar(1900).IsSchrikkeljaar);
+            Assert.IsFalse(new Jaar(1900).IsSchrikkeljaar);
         }
         [TestMethod]
         public void EenJaarDeelbaarDoor4IsEenSchrikkeljaar()
         {
-            Assert.AreEqual(true, new Jaar(2012).IsSchrikkeljaar);
+            Assert.IsTrue(new Jaar(2012).IsSchrikkeljaar);
         }
         [TestMethod]
         public void EenJaarNietDeelbaarDoor4IsGeenSchrikkeljaar()
         {
-            Assert.AreEqual(false, new Jaar(2015).IsSchrikkeljaar);
+            Assert.IsFalse(new Jaar(2015).IsSchrikkeljaar);
         }
     }
 }
