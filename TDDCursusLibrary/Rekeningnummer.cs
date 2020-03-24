ec6136220410c35664b5e6d14670552e03bb66f3
ZegMaarJoe <willaertjonathan@gmail.com>
2020-03-24 16:42:08 +0100

    RekeningnummerTest Rekeningnummer code toegevoegd Alle tests slagen

---
+++ TDDCursusLibrary/Rekeningnummer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TDDCursusLibrary
+{
+    public class Rekeningnummer
+    {
+        private static readonly Regex regex = new Regex("^\\d{3}-\\d{7}-\\d{2}$");
+        private readonly string nummer;
+        public Rekeningnummer(string nummer)
+        {
+            //  throw new NotImplementedException();
+            if (!regex.IsMatch(nummer))
+            {
+                throw new ArgumentException();
+            }
+
+            var eerste10Cijfers = long.Parse(nummer.Substring(0, 3) + nummer.Substring(4, 7));
+            var laaste2Cijfers = long.Parse(nummer.Substring(12, 2));
+
+            if (eerste10Cijfers % 97 != laaste2Cijfers)
+            {
+                throw new ArgumentException();
+            }
+
+            this.nummer = nummer;
+        }
+        public override string ToString()
+        {
+            //  throw new NotImplementedException();
+            return nummer;
+        }
+    }
+}
