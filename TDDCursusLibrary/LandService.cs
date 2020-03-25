586f8fd4677b477aeb1f2f0bea7c6cb5fe5dc6dc
ZegMaarJoe <willaertjonathan@gmail.com>
2020-03-25 17:29:39 +0100

    Gebruik van een stub (maakt een mock up object om te kunnen testen zonder alle classen geschreven moeten zijn)

---
+++ TDDCursusLibrary/LandService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDCursusLibrary
+{
+    public class LandService
+    {
+        // Verwijzing naar Interface
+        private readonly ILandDAO landDAO;
+        // Constructor
+        public LandService(ILandDAO landDAO)
+        {
+            this.landDAO = landDAO;
+        }
+        public decimal VindVerhoudingOppTotOppVanAlleLanden(string landcode)
+        {
+            var land = landDAO.Read(landcode);
+            var oppvervlakteAlleLanden = landDAO.OppervlakteAlleLanden();
+            return (decimal)land.Oppervlakte / oppvervlakteAlleLanden;
+        }
+    }
+}
---
+++ TDDCursusLibraryTest/LandServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDCursusLibrary;
+
+namespace TDDCursusLibraryTest
+{
+    [TestClass]
+    public class LandServiceTest
+    {
+        private ILandDAO landDAO;
+        private LandService landService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            landDAO = new LandDAOStub();
+            landService = new LandService(landDAO);
+        }
+
+        [TestMethod]
+        public void VindVerhoudingOppLandTotOppAlleLanden()
+        {
+            Assert.AreEqual(0.25m, landService.VindVerhoudingOppTotOppVanAlleLanden("B"));
+        }
+    }
+}
