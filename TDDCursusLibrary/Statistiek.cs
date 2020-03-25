ecf89c5ad95dc3cdb052ffe123973f4f235649f9
ZegMaarJoe <willaertjonathan@gmail.com>
2020-03-25 15:26:32 +0100

    Taak ISBN

---
+++ TDDCursusLibrary/Statistiek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace TDDCursusLibrary
+{
+    public class Statistiek
+    {
+        public static decimal Gemiddelde(decimal[] getallen)
+        {
+            //  throw new NotImplementedException();
+            if (getallen == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (getallen.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return getallen.Average();
+        }
+    }
+}
